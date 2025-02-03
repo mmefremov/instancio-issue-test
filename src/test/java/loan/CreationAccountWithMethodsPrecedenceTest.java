87ac865def47a4659d19d2ccc4ee8b31a38b9585
Hungryas <hungryas@gmail.com>
2025-02-04 01:19:49 +0300

    add test case for the methods precedence

--- src/test/java/loan/CreationAccountWithMethodsPrecedenceTest.java
+++ src/test/java/loan/CreationAccountWithMethodsPrecedenceTest.java
@@ -5,9 +5,10 @@ import org.assertj.core.api.Assertions;
 import org.instancio.Assign;
 import org.instancio.Instancio;
 import org.instancio.Model;
-import org.instancio.Select;
 import org.instancio.core.model.Account;
 import org.junit.jupiter.api.Test;
+
+import static org.instancio.Select.field;
 
 @Slf4j
 class CreationAccountWithMethodsPrecedenceTest {
@@ -15,16 +16,18 @@ class CreationAccountWithMethodsPrecedenceTest {
     @Test
     void testCreationAccountWithMethodsPrecedenceChange() {
         Model<Account> accountModel = Instancio.of(Account.class)
-                .set(Select.field(Account::getNumber), "001")
-                .set(Select.field(Account::getOrder), 1)
+                .set(field(Account::getNumber), "001")
+                .set(field(Account::getOrder), 1)
                 .toModel();
         Account account = Instancio.of(accountModel)
+                .supply(field(Account::getNumber), random -> random.digits(10))
                 .assign(Assign.valueOf(Account::getNumber).generate(
                         gen -> gen.string().digits().length(3)))
-                .generate(Select.field(Account::getOrder), gen -> gen.ints().min(2))
+                .generate(field(Account::getOrder), gen -> gen.ints().min(2))
                 .create();
         log.info("request with single account: {}", account);
-        Assertions.assertThat(account.getNumber()).as("account number").isNotEqualTo("001");
+        Assertions.assertThat(account.getNumber()).as("account number").isNotEqualTo("001")
+                .hasSize(3);
         Assertions.assertThat(account.getOrder()).as("account order").isOne();
     }
 }
